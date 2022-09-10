348d9e672f3ae0a23561b3ddca6892f033945b86
Konstantin <tumandurman96@gmail.com>
2022-09-10 15:49:06 +0300

    Seminar4CW123

--- Csharp/Seminar/Seminar4/Seminar4CW2/Program.cs
+++ Csharp/Seminar/Seminar4/Seminar4CW2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// создаем рабочий алгоритм, переводим его в метод
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int CountDigit(int number)
+{
+int count = 0;
+if (number ==0)
+{
+    return 1;
+}
+while (number != 0)
+{
+    number = number / 10;
+    count++;
+}
+return count;
+}
+int number = Prompt("Введите число ->");
+System.Console.WriteLine(CountDigit(number));
+
+
+
+
+
+// int number = Prompt("Введите число ->");
+// int count = 0;
+// while (number != 0) // выпоолняется цикл пока вып усл
+//{
+//    number = number / 10;
+ //   count++;
+//}
+//System.Console.WriteLine(count);
