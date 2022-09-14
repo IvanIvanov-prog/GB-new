807679737ea940ff8780658b813de844f2c398b0
Konstantin <tumandurman96@gmail.com>
2022-09-14 21:36:54 +0300

    FailTask3Task4

---
+++ Csharp/ClassworkCsh/ClassworkCsh4/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// нахождение числа Фибонатче
+// f(1) = 1
+// f(2) = 1
+// f(3) = f(n-1) + f(n-2)
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 50; i++) // расчет числа Ф для первых десяти/ сорока
+{
+    Console.WriteLine($"F({i} = {Fibonacci(i)}");
+}
