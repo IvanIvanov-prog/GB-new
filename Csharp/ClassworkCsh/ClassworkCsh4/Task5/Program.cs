5d52e6d5ea194cb4c0fc9c9a17e069ad39a187d4
Konstantin <tumandurman96@gmail.com>
2022-09-14 21:56:24 +0300

    Task5

--- Csharp/ClassworkCsh/ClassworkCsh4/Task5/Program.cs
+++ Csharp/ClassworkCsh/ClassworkCsh4/Task5/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Рекурсивные методы игра тетрис
+// двумерный массив - поле для игры в котором  0 свободные клетки 1 занятые клетки
+// движенгие фигуры смещение 1 и 0 относительно ранее занятых клеток
+// поворот фигуры - поворот матрицы // Куски кода для начала
+void Figure(int x, int y)
+{
+  Console.Clear();
+  for (int i = x - 1; i <= x + 1; i++)
+  {
+    for (int j = y - 1; j <= y + 1; j++)
+    {
+      Console.SetCursorPosition(i, j);
+      Console.Write("+");
+    }
+  }
+}
+int x = 10;
+int y = 2;
+
+// Логика отрисовки всего
+new Thread(() =>
+{
+  while (true)
+  {
+    Figure(x, y);
+    Thread.Sleep(500);
+    y++;
+    if (y > 15) y = 1;
+  }
+}).Start();
+
+// Логика проверки нажатия кнопок
+while (true)
+{
+  var key = Console.ReadKey(true).Key;
+
+  if (key == ConsoleKey.LeftArrow)
+  {
+    x--;
+    Figure(x, y);
+  }
+  if (key == ConsoleKey.RightArrow)
+  {
+    x++;
+    Figure(x, y);
+  }
+}
