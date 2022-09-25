29495e58c8099ddbd0ba15dac95306f794bf61d6
Konstantin <tumandurman96@gmail.com>
2022-09-25 15:52:30 +0300

    Seminar9Task1-8

---
+++ Csharp/Seminar/Seminar9/Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Определите, является ли число степенью двойки:
+N = 16 -> "Является степень двойки"
+N = 12 -> “Не является степенью двойки”
+*/
+bool Check(int a)
+{
+    if (a % 2 == 1)
+    {
+        return false;
+    }
+    if (a == 2)
+    {
+        return true;
+    }
+
+    return Check(a / 2);
+}
+bool result = Check(6);
+System.Console.WriteLine(result);
+
