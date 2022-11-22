1b11bed967066db092efd86ee505514d7f7a48f9
Konstantin <tumandurman96@gmail.com>
2022-11-22 16:45:58 +0300

    PythonSminarTask12345

---
+++ Python/ClassWorks/Lerciya1/Lec1.py
@@ -0,0 +1,28 @@
+# Типы данных и переменная
+# int, float, boolean, str, list, None
+value = None
+a = 123
+b = 1.23
+print(type(a))
+print(type(b))
+value = 1244
+print (type(value))
+s = 'Hello World'
+print(s) # Вывод строки
+print(a, ' _ ', b, s) 
+print('{1} - {2} - {0}'.format(a, b, s))
+print(f'{a} - {b} - {s}')
+
+f = True
+print(f) 
+
+# Массив = Список
+List = [1,2,3]
+print(list)
+
+#Ввод и вывод
+print ('Введиите a');
+a = input()
+print ('Ввдите b');
+b = input()
+print(a, b)
